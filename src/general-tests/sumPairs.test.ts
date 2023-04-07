9376367c1f4ee1ab867ccf6ba437d21431b8b2c7
Juan Sebastian Gonzalez <3393@holbertonstudents.com>
2023-04-07 14:11:15 -0500

    Implementing sumPairs without t20 pointers or hashmap

---
+++ src/general-tests/sumPairs.test.ts
@@ -0,0 +1,63 @@
+const sumPairs = (numbers: number[], result: number) => {
+  if (typeof numbers === 'undefined')
+    throw new Error('first argument is missing');
+  if (Array.isArray(numbers) === false)
+    throw new Error('first argument must be a list');
+  if (numbers.some((num) => typeof num !== 'number'))
+    throw new Error('list must contain only numbers');
+  if (typeof result !== 'number')
+    throw new Error('second argument is missing or is not a number');
+  if (numbers.length < 2) return null;
+
+  for (let i = 0; i < numbers.length; i++) {
+    let difference = result - numbers[i];
+    if (numbers.indexOf(difference, i + 1) !== -1)
+      return [numbers[i], difference];
+  }
+  return null;
+};
+
+describe('sumPairs', () => {
+  test('sumPairs to be defined', () => {
+    expect(sumPairs).toBeDefined();
+  });
+  test('should be a function', () => {
+    expect(typeof sumPairs).toBe('function');
+  });
+  test('should throw an error if the argument is missing', () => {
+    expect(() => sumPairs()).toThrow();
+  });
+  test('should throw an error with text first included if the first argument is missing', () => {
+    expect(() => sumPairs()).toThrow(/first/);
+  });
+  test('should throw an error if the first argument is not a list', () => {
+    expect(() => sumPairs('hello')).toThrow(/list/);
+  });
+  test('should thow an error if the lists doesnt have numbers', () => {
+    expect(() => sumPairs(['hello'])).toThrow(/number/);
+  });
+  test('should throw an error if the second argument is missing', () => {
+    expect(() => sumPairs([1, 2])).toThrow(/second/);
+  });
+  test('should return null if the length of the first argument is less than 2', () => {
+    expect(sumPairs([1], 2)).toBeNull();
+  });
+  test('should return [3,7]  if arguments are [3, 5, 7, 2] and 10', () => {
+    expect(sumPairs([3, 5, 7, 2], 10)).toEqual([3, 7]);
+  });
+  test('sumPairs([-3, -2, 7, -5], 10)should return null', () => {
+    expect(sumPairs([-3, -2, 7, -5], 10)).toBeNull();
+  });
+  test('sumPairs([0, 2, 2, 3, -1, 1, 5], 6)should return [1,5]', () => {
+    expect(sumPairs([0, 2, 2, 3, -1, 1, 5], 6)).toEqual([1, 5]);
+  });
+  test('sumPairs([0, 2, 2, 3, -1, 1, 9], 6)should return null', () => {
+    expect(sumPairs([0, 2, 2, 3, -1, 1, 9], 6)).toBeNull();
+  });
+  test('sumPairs([1,5], 6)should return [1,5]', () => {
+    expect(sumPairs([1, 5], 6)).toEqual([1, 5]);
+  });
+  test('should return the pair that is at the left', () => {
+    expect(sumPairs([6, 7, 1, 2], 8)).toEqual([6, 2]);
+  });
+});
